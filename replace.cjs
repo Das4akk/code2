fb196ae07f8b4009d3d84f7ef07bd9a4f70f2f5c
Das4akk <malashchenko.danya@mail.ru>
2026-06-01 15:08:35 +0300

    .

--- replace.cjs
+++ replace.cjs
@@ -1,30 +1,32 @@
 const fs = require('fs');
-let appJs = fs.readFileSync('app.js', 'utf8');
-let indexHtml = fs.readFileSync('index.html', 'utf8');
 
-const replacements = [
-    { from: 'https://em-content.zobj.net/source/telegram/386/star_2b50.webp', to: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/2b50.png' },
-    { from: 'https://em-content.zobj.net/source/apple/391/fire_1f525.png', to: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f525.png' },
-    { from: 'https://em-content.zobj.net/source/apple/391/gem-stone_1f48e.png', to: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f48e.png' },
-    { from: 'https://em-content.zobj.net/source/apple/391/crown_1f451.png', to: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f451.png' },
-    { from: 'https://cdn.emoji.gg/emojis/3468-love.gif', to: 'https://media.giphy.com/media/l4pTdcifPZLpDjL1e/giphy.gif' },
-    { from: 'https://cdn.emoji.gg/emojis/5232-heart.gif', to: 'https://media.giphy.com/media/26BRv0ThflsHCqDrG/giphy.gif' },
-    { from: 'https://cdn.emoji.gg/emojis/4638-heart.gif', to: 'https://media.giphy.com/media/LpDmM2wSt6Hm5fKJVa/giphy.gif' },
-    { from: 'https://cdn.emoji.gg/emojis/7697-ring.gif', to: 'https://media.giphy.com/media/Mous21IAhJQiI/giphy.gif' },
-    { from: 'https://cdn.emoji.gg/emojis/1690-love-face-emoji.gif', to: 'https://media.giphy.com/media/MDJ9IbxxvDUQM/giphy.gif' },
-    { from: 'https://em-content.zobj.net/source/telegram/386/fire_1f525.webp', to: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f525.png' },
-    { from: 'https://em-content.zobj.net/source/telegram/386/shopping-bags_1f6cd-fe0f.webp', to: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f6cd.png' },
-    { from: 'https://em-content.zobj.net/source/telegram/386/face-with-tears-of-joy_1f602.webp', to: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f602.png' },
-    { from: 'https://em-content.zobj.net/source/telegram/386/face-screaming-in-fear_1f631.webp', to: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f631.png' },
-    { from: 'https://em-content.zobj.net/source/telegram/386/red-heart_2764-fe0f.webp', to: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/2764.png' },
-    { from: 'https://em-content.zobj.net/source/telegram/386/clapping-hands_1f44f.webp', to: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f44f.png' },
-    { from: 'https://em-content.zobj.net/source/telegram/386/musical-notes_1f3b6.webp', to: 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f3b6.png' }
-];
+const mappings = {
+    'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f510.png': 'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Objects/Locked%20With%20Key.webp',
+    'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f468.png': 'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/People/Man%20Technologist.webp',
+    'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f469.png': 'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/People/Woman%20Technologist.webp',
+    'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f319.png': 'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Animals%20and%20Nature/First%20Quarter%20Moon%20Face.webp',
+    'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f504.png': 'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Symbols/Up%20Button.webp',
+    'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f6aa.png': 'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Travel%20and%20Places/House.webp',
+    'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f4ce.png': 'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Objects/Briefcase.webp',
+    'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f4cc.png': 'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Objects/Reminder%20Ribbon.webp',
+    'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/2699.png': 'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Objects/Toolbox.webp',
+    'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/1f42e.png': 'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Animals%20and%20Nature/Cow.webp'
+};
 
-for (let r of replacements) {
-    appJs = appJs.split(r.from).join(r.to);
-    indexHtml = indexHtml.split(r.from).join(r.to);
+function processFile(filename) {
+    let content = fs.readFileSync(filename, 'utf8');
+    let mod = false;
+    for (const [oldUrl, newUrl] of Object.entries(mappings)) {
+        if (content.includes(oldUrl)) {
+            content = content.split(oldUrl).join(newUrl);
+            mod = true;
+            console.log(`Replaced in ${filename}: ${oldUrl.split('/').pop()} -> ${newUrl.split('/').pop()}`);
+        }
+    }
+    if (mod) {
+        fs.writeFileSync(filename, content);
+    }
 }
 
-fs.writeFileSync('app.js', appJs);
-fs.writeFileSync('index.html', indexHtml);
+processFile('index.html');
+processFile('app.js');
